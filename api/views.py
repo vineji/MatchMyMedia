c120fbba40a2ad6baef90bda1cac7c2300aa7f8e
vineji <vineji7@outlook.com>
2025-01-26 16:02:13 +0000

    added view for book search

--- api/views.py
+++ api/views.py
@@ -7,6 +7,11 @@ from rest_framework import status
 
 import requests
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def main_spa(request):
     return render(request, 'index.html')
@@ -20,7 +25,7 @@ class movie_search(APIView):
         if not title:
             return Response({'movies':[]}, status=status.HTTP_400_BAD_REQUEST)
         
-        api_key = '75c89f5935bea75e58f550b0d0476ad2'
+        api_key = os.getenv("TMBD_API_KEY")
         url = f'https://api.themoviedb.org/3/search/movie?query={title}&api_key={api_key}&page={page_number}'
 
         response = requests.get(url)
@@ -48,7 +53,7 @@ class show_search(APIView):
         if not title:
             return Response({'shows':[]}, status=status.HTTP_400_BAD_REQUEST)
         
-        api_key = '75c89f5935bea75e58f550b0d0476ad2'
+        api_key = os.getenv("TMBD_API_KEY")
         url = f'https://api.themoviedb.org/3/search/tv?query={title}&api_key={api_key}&page={page_number}'
 
         response = requests.get(url)
@@ -66,3 +71,36 @@ class show_search(APIView):
             return Response(response_data)
         else:
             return Response({'shows':[]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+class book_search(APIView):
+    def get(self,request,format=None):
+
+        title = request.GET.get('title', '')
+        page_number = request.GET.get('page', 1)
+        startIndex = 10 * (page_number - 1)
+        maxItemsPerPage = 20
+
+        if not title:
+            return Response({'items':[]}, status=status.HTTP_400_BAD_REQUEST)
+        
+        api_key = os.getenv("GOOGLE_BOOKS_API_KEY")
+        url = f'https://www.googleapis.com/books/v1/volumes?q={title}&startIndex={startIndex}&maxResults={maxItemsPerPage}&key={api_key}'
+
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            data = response.json()
+            books = data['items']
+            total_pages = data['totalItems'] // maxItemsPerPage
+            current_page = page_number
+
+            response_data = {
+                'books': books,
+                'total_pages': total_pages,
+                'current_page': current_page
+            }
+            return Response(response_data)
+        else:
+            return Response({'books':[]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
