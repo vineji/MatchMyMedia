b3907f19a664b6342bc4e018309d8aebc0f80952
vineji <vineji7@outlook.com>
2025-02-14 15:47:36 +0000

    Recommendation algorithm extracts keywords from description and ranks the books return by the API by comparing the Cosine Similarity

--- api/recommendation.py
+++ api/recommendation.py
@@ -3,8 +3,8 @@ import spacy
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sentence_transformers import SentenceTransformer, util
-from suprise import SVD, Dataset, Reader
-from suprise.model_selection import train_test_split
+from surprise import SVD, Dataset, Reader
+from surprise.model_selection import train_test_split
 
 import os
 from dotenv import load_dotenv
@@ -18,59 +18,102 @@ model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def fetch_books_from_google_api(query):
     
-        api_key = os.getenv("TMBD_API_KEY")
+    api_key = os.getenv("GOOGLE_BOOKS_API_KEY")
 
-        recommendations = []
+    recommendations = []
 
-        start_index = 0
+    start_index = 0
 
-        excluded_categories = ['Cooking','Self-Help','Health & Fitness','Hobbies','Crafts',
-                               'Gardening','Travel','Art','Photography']
-        
+    excluded_categories = ['Cooking','Self-Help','Health & Fitness','Hobbies','Crafts',
+                            'Gardening','Travel','Art','Photography']
+    
+    max_results = 40
+    
 
-        try:
+    try:
 
-            while len(recommendations) < max_results:
+        while len(recommendations) < max_results:
 
-                url = "https://www.googleapis.com/books/v1/volumes"
-                query_string = f"{query['title']} {' '.join(query['keywords'])} {query['genre']}"
+            url = "https://www.googleapis.com/books/v1/volumes"
+            query_string = f"{query['title']} {' '.join(query['keywords'])} {query['genre']}"
 
-                param_list = {
-                     "q" : query_string,
-                     "maxResults" : 40,
-                     "startIndex" : start_index,
-                     "key" : api_key
-                }
+            param_list = {
+                    "q" : query_string,
+                    "maxResults" : 40,
+                    "startIndex" : start_index,
+                    "key" : api_key
+            }
 
-                response = requests.get(url, params=param_list)
-                response.raise_for_status()
+            response = requests.get(url, params=param_list)
+            response.raise_for_status()
 
-                books_data = response.json()
+            books_data = response.json()
 
-                for book in books_data.get("items", []):
-                    volumeInfo = book.get("volumeInfo", {})
-                    categories = volumeInfo.get("categories", [])
+            for book in books_data.get("items", []):
+                volumeInfo = book.get("volumeInfo", {})
+                categories = volumeInfo.get("categories", [])
 
-                    if not categories or any(category in excluded_categories for category in categories):
-                        continue
-                
-                    recommendations.append({
-                        "title" : volumeInfo.get("title", "Unknown Title"),
-                        "authors" : volumeInfo.get("authors", ["Unknown Author"]),
-                        "description" : volumeInfo.get("description", "No Description Available"),
-                        "categories" : categories,
-                        "image" : volumeInfo.get("imageLinks", {}).get("thumbnail")
-                    })
-                
-                start_index += len(books_data.get("items", []))
-                if len(books_data.get("items", [])) < 40:
-                     break
-            return recommendations[:40]
-        
-        except Exception as e:
-            print(f"Error fetching books: {e}")
-            return []
-        
+                if not categories or any(category in excluded_categories for category in categories):
+                    continue
+            
+                recommendations.append({
+                    "title" : volumeInfo.get("title", "Unknown Title"),
+                    "authors" : volumeInfo.get("authors", ["Unknown Author"]),
+                    "description" : volumeInfo.get("description", "No Description Available"),
+                    "categories" : categories,
+                    "image" : volumeInfo.get("imageLinks", {}).get("thumbnail")
+                })
+            
+            start_index += len(books_data.get("items", []))
+            if len(books_data.get("items", [])) < 40:
+                    break
+        return recommendations[:40]
+    
+    except Exception as e:
+        print(f"Error fetching books: {e}")
+        return []
+
+
+def extract_keywords(text, n=5):
+    
+    doc = nlp(text)
+    words = [token.lemma_ for token in doc if token.is_alpha and not token.is_stop]
+
+    vectorizer = TfidfVectorizer(stop_words='english')
+    tfIdf_matrix = vectorizer.fit_transform([" ".join(words)])
+    feature_arr = vectorizer.get_feature_names_out()
+    tfIdf_sorting = tfIdf_matrix.toarray().flatten().argsort()[::-1]
+
+
+    return [feature_arr[i] for i in tfIdf_sorting[:n]]
+
+def rank_books_by_cosine_similarity(movie_query, books):
+     
+    movie_embedding = model.encode(movie_query["description"], convert_to_tensor=True)
+
+    ranked_books = []
+
+    for book in books:
+        book_embedding = model.encode(book["description"], convert_to_tensor=True)
+        similarity_score = util.pytorch_cos_sim(movie_embedding, book_embedding)
+        ranked_books.append((book,similarity_score))
+    
+    ranked_books.sort(key=lambda x: x[1], reverse=True)
+    return [book[0] for book in ranked_books]
+    
+
+def get_recommended_books(movie_query):
+
+    movie_query["keywords"] = extract_keywords(movie_query["description"])
+
+    books = fetch_books_from_google_api(movie_query)
+
+    ranked_book_recommendations = rank_books_by_cosine_similarity(movie_query, books)
+
+    return ranked_book_recommendations
+
+
+
                 
 
 
